6a960891ce0281ab39d09c2e2cf8d6ee4660948f
Nilton <cesar.n.2@hotmail.com>
2022-12-20 22:38:58 -0300

    configuraçao do json

--- Req_http/httpreact/src/App.js
+++ Req_http/httpreact/src/App.js
@@ -1,11 +1,36 @@
+import {useState, useEffect} from "react"
 
-import './App.css';
+const url = "http://localhost:3000/products"
 
 function App() {
+
+  // 1-> resgatando dados
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData();
+  }, []);
+
+  console.log(products)
+
+  
+
   return (
     <div>
       <h1>Oi</h1>
-      <h1>oi</h1>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name} - {product.price}</li>
+        ))}
+      </ul>
     </div>
   );
 }
