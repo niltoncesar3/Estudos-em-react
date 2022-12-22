06886f8081c5890a26a9d2d65a8b5aafedf36f1f
Nilton <cesar.n.2@hotmail.com>
2022-12-21 21:30:21 -0300

    requisição http

--- Req_http/httpreact/src/App.js
+++ Req_http/httpreact/src/App.js
@@ -4,33 +4,30 @@ const url = "http://localhost:3000/products"
 
 function App() {
 
-  // 1-> resgatando dados
+  //1-> Resgatando dados
+
   const [products, setProducts] = useState([])
 
   useEffect(() => {
-    async function fetchData() {
+    async function fecthData() {
       const res = await fetch(url);
-
       const data = await res.json();
 
-      setProducts(data);
+      setProducts(data)
     }
 
-    fetchData();
-  }, []);
+    fecthData();
+  },[])
 
   console.log(products)
-
   
+  // 2 -> Adição de produtos
+
 
   return (
     <div>
       <h1>Oi</h1>
-      <ul>
-        {products.map((product) => (
-          <li key={product.id}>{product.name} - {product.price}</li>
-        ))}
-      </ul>
+
     </div>
   );
 }
