754b809544c6fb7612739e1f7d0c5b34443cb642
Nilton <cesar.n.2@hotmail.com>
2023-01-05 23:14:05 -0300

    change useeffect

--- Req_http/httpreact/src/App.js
+++ Req_http/httpreact/src/App.js
@@ -1,18 +1,13 @@
-import { useState, useEffect } from "react";
-
-//components
-
+import {useState, useEffect} from "react"
 
 const url = "http://localhost:3000/products"
 
+
 function App() {
+
   const [products, setProducts] = useState([])
 
-  const [name, setName] = useState('')
-  const [price, setPrice] = useState('')
-   
-
-  //1-> Resgatando dados
+  // 1 -> Resgatando dados
   useEffect(() => {
     async function fetchData() {
       const res = await fetch(url)
@@ -23,62 +18,20 @@ function App() {
 
     fetchData()
   },[])
-  
-  
-  // 2 -> Adição de produtos
-  const handleSubmit = async (e) => {
-    e.preventDefault();
 
-    const product = {
-      name,
-      price,
-    };
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type":"application/json"
-      },
-      body: JSON.stringify(product),
-    });
-
-     // 3 -> carregamento dinamico
-    const addedProduct =  await res.json();
-
-    setProducts((prevProducts) => [...prevProducts,addedProduct])
-
-    setProducts("")
-    setPrice("")
-  }
 
 
   return (
     <div>
       <ul>
         {products.map((product) => (
-          <li key={product.id}>{product.name} R$ {product.price}</li>
+          <li key={product.id}>
+            {product.name} {product.price}
+          </li>
         ))}
       </ul>
-      <div className="add-products">
-        <form onSubmit={handleSubmit}>
-          <label>
-            Nome: 
-            <input type="text"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            />
-          </label>
-          <label>
-            Preço: 
-            <input type="number"
-            name="name"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            />
-          </label>
-          <input type="submit"/>
-        </form>
-      </div>
+     
+            
       
 
     </div>
